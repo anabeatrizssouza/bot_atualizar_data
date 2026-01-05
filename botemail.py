e3515a9a6093f478db30ba376a64970c9336472b
Ana Beatriz Silva Souza <167987389+anabeatrizssouza@users.noreply.github.com>
2026-01-05 14:13:20 -0300

    Update botemail.py

--- botemail.py
+++ botemail.py
@@ -2,15 +2,17 @@ import pyautogui
 import time
 import mouseinfo
 import pyperclip
+import pyscreeze
 
-
+pyautogui.FAILSAFE = True
 primCel = 463,297
-total_int = 1265
+total_int = 1160
 contador = 0
 
 pyautogui.click(primCel, duration=0.5)
 time.sleep(0.3)
 
+pyautogui.press('right')
 pyautogui.press('right')
 pyautogui.press('right')
 pyautogui.press('right')
@@ -42,42 +44,53 @@ while contador < total_int:
     
     pyautogui.hotkey('ctrl','c')
     time.sleep(1)
-    pyautogui.click(1247,1058)
+    pyautogui.click(1097,1060)
     time.sleep(0.3)
     pyautogui.hotkey('ctrl','l')
-    pyautogui.moveTo(925,684)
+    pyautogui.moveTo(997,667)
     pyautogui.doubleClick()
     time.sleep(0.3)
     pyautogui.hotkey('ctrl','v')
     time.sleep(0.3)
-    pyautogui.click(162,304, duration=0.3)
+    pyautogui.doubleClick(147,324, duration=0.5)
+    time.sleep(2)
     #localizar
-    pyautogui.click(1313,686, duration=0.3)
+    pyautogui.click(1331,667, duration=0.3)
     time.sleep(8)
-    pyautogui.click(1097,1061, duration=0.3)
+    pyautogui.click(301,350, duration=0.3)
+    time.sleep(1)
+    pyautogui.click(65,448, duration=0.3)
+    time.sleep(0.5)
+    pyautogui.click(65,637, duration=0.5)
+    time.sleep(2)
+    pyautogui.click(375,429, duration=0.3)
+    time.sleep(0.5)
+    pyautogui.click(180,549, duration=0.3)
+    time.sleep(0.5)
+    pyautogui.doubleClick(405,426, duration=0.3)
+    time.sleep(0.9)
+    pyautogui.write('010126')
+    time.sleep(0.9)
+    pyautogui.doubleClick(482,426, duration=0.3)
+    time.sleep(0.9)
+    pyautogui.write('311226')
+    pyautogui.click(505,116, duration=0.5)
+    time.sleep(2)
+    pyautogui.click(1006,1056, duration=0.3)
+    time.sleep(2)
+    
     pyautogui.press('right')
     pyautogui.press('right')
-    pyautogui.hotkey('ctrl','c')
-    time.sleep(0.3)
-    pyautogui.click(1247,1058, duration=0.3)
-    pyautogui.click(443,662, duration=0.3)
-    time.sleep(0.3)
-    pyautogui.moveTo(114,663, duration=0.3)
-    pyautogui.doubleClick()
-    time.sleep(0.3)
-    pyautogui.press('delete', presses=150)
-    time.sleep(0.3)
-    pyautogui.hotkey('ctrl','v')
-    pyautogui.click(522,104, duration=0.3)
-    pyautogui.click(1095,1061, duration=0.3)
-    pyautogui.click(1250,8, duration=0.3)
     pyautogui.press('right')
-    pyautogui.write("OK")
 
+    pyautogui.write("ok")
+    
     pyautogui.press('down')
     pyautogui.press('left')
     pyautogui.press('left')
     pyautogui.press('left')
+
+    time.sleep(1)
 
     contador+=1
 
